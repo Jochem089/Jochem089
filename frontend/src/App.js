2d01121906f3fe5be65dd5f12e877a955ca7c251
Jochem van Dillen <jochem.van.dillen@hva.nl>
2024-02-13 15:50:38 +0100

    setup react front-end

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Welcome to my site!</h1>
+                <p>This is a simple test project</p>
+            </header>
+        </div>
+    );
+}
+
+export default App;
