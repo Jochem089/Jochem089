ad2529a557e2c86a4d0127fe53c4591818a72681
Jochem van Dillen <jochem.van.dillen@hva.nl>
2026-01-19 23:04:28 +0100

    changed navbar

--- frontend/src/components/navbar/NavBar.js
+++ frontend/src/components/navbar/NavBar.js
@@ -5,8 +5,8 @@ import {Link} from "react-router-dom"
 function NavBar() {
     return (
         <nav>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About</Link></li>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
         </nav>
     )
 }
