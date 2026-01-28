9251e66878cb89d866969f89e991a1e8db9f7835
Jochem van Dillen <jochem.van.dillen@hva.nl>
2026-01-28 14:41:47 +0100

    added tailwind

--- frontend/src/components/home/Homepage.js
+++ frontend/src/components/home/Homepage.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
+import {Button} from "@mui/material";
 
 function HomePage() {
+    const [count, setCount] = useState(0)
+
+    function handleClick() {
+        console.log("clicked");
+        setCount(count + 1)
+    }
+
     return (
-        <div className="container">
-            <div className="content">
+        <div className="flex justify-center">
+            <div className="flex flex-col gap-4">
                 <h1>Welcome to my site!</h1>
                 <p>This is a simple test project</p>
+                <Button variant="contained" onClick={handleClick}>Click me!</Button>
+                <p>Counter: { count }</p>
             </div>
         </div>
     )
